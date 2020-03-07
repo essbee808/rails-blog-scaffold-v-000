48b52f092a1705e633508a6b5e431bc984134955
Sheena Bonilla <sheena.myb@gmail.com>
2020-03-07 04:55:44 +0000

    Automatically backed up by Learn

--- spec/views/posts/new.html.erb_spec.rb
+++ spec/views/posts/new.html.erb_spec.rb
@@ -1,10 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe "posts/new", :type => :view do
-  before(:each) do
-    assign(:post, Post.new(
-      :title => "MyString"
-    ))
+RSpec.describe "posts/new", :type => :view do before(:each) do
+    assign(:post, Post.new(:title => "MyString"))
   end
 
   it "renders new post form" do
